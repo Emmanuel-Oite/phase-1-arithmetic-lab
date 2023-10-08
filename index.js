adc2d91e3eee2efc4afacf8b8bf3f0d4af6044a3
Emmanuel <opotiemmanuel@gmail.com>
2023-10-08 16:51:56 +0300

    Completed ASSIGNMENT

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
+let random;
+random = Math.floor(Math.random(0)*6)+1;
 // Write your code here
+let multiply;
+let mod;
+let max;
+let num1 = 31;
+let num2 = 2;
+let num3 = 40;
+let num4 = 10; 
+multiply = num1 * num2;
+mod = num3 / num4;
+max = Math.max(5, 10, 20);
